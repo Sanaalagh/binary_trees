74fe2b2a6b74c7260f2b22f54959575379147cc9
Sanaalagh <Sanaalaghzou@gmail.com>
2024-02-27 13:33:34 -0800

    fix the code again

--- binary_trees.h
+++ binary_trees.h
@@ -18,6 +18,7 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /* Function Prototypes */
+size_t binary_tree_size(const binary_tree_t *tree);
 
 /* Basic operations */
 void binary_tree_print(const binary_tree_t *);
