7e9b437a0e1faaca6cf5ea22b8cdbf54d7abea59
Sanaalagh <Sanaalaghzou@gmail.com>
2024-02-29 17:12:59 -0800

    fix the code

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,59 +1,86 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * bst_remove - Removes a node from a Binary Search Tree
- * @root: A pointer to the root node of the tree where to remove a node
- * @value: The value to remove from the tree
+ * transplant - Replaces one subtree as a child of its parent .
+ * @root: double pointer to the root node of the tree
+ * @u: pointer to the node to be replaced
+ * @v: pointer to the node to replace with
  *
- * Return: A pointer to the new root node of the tree after removing the
- *         desired value
+ * Description: This function replaces the subtree rooted at node `u` with
+ * the subtree rooted at node `v`. `u`'s parent becomes `v`'s parent, and `u`'s
+ * parent ends up having `v` as its appropriate child.
+ */
+void transplant(bst_t **root, bst_t *u, bst_t *v)
+{
+	if (!u->parent)
+		*root = v;
+	else if (u == u->parent->left)
+		u->parent->left = v;
+	else
+		u->parent->right = v;
+	if (v)
+		v->parent = u->parent;
+}
+
+/**
+ * tree_minimum - Finds the minimum value in a binary search tree.
+ * @node: pointer to the root node of the tree to search
+ *
+ * Return: pointer to the node containing the minimum value
+ */
+bst_t *tree_minimum(bst_t *node)
+{
+	while (node->left != NULL)
+		node = node->left;
+	return (node);
+}
+
+/**
+ * bst_remove - Removes a node from a Binary Search Tree.
+ * @root: pointer to the root node of the tree where you will remove a node.
+ * @value: The value to remove in the tree.
+ *
+ * Return: pointer to the new root node of
+ *  the tree after removing the desired value.
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-    bst_t *temp;
+	if (!root)
+		return (NULL);
 
-    if (root == NULL)
-        return NULL;
+	bst_t *temp, *node_to_remove = root;
 
-    /* If the value to be removed is smaller than the root value,
-       it's in the left subtree */
-    if (value < root->n)
-        root->left = bst_remove(root->left, value);
+	while (node_to_remove && node_to_remove->n != value)
+	{
+		if (value < node_to_remove->n)
+			node_to_remove = node_to_remove->left;
+		else
+			node_to_remove = node_to_remove->right;
+	}
 
-    /* If the value to be removed is greater than the root value,
-       it's in the right subtree */
-    else if (value > root->n)
-        root->right = bst_remove(root->right, value);
+	if (!node_to_remove)  /* Node not found */
 
-    /* If the value to be removed is equal to the root value */
-    else
-    {
-        /* If the root has no children or only one child */
-        if (root->left == NULL)
-        {
-            temp = root->right;
-            free(root);
-            return temp;
-        }
-        else if (root->right == NULL)
-        {
-            temp = root->left;
-            free(root);
-            return temp;
-        }
+		return (root);
 
-        /* If the root has two children,
-           find the in-order successor (smallest in the right subtree) */
-        temp = root->right;
-        while (temp->left != NULL)
-            temp = temp->left;
+	if (!node_to_remove->left)
+		transplant(&root, node_to_remove, node_to_remove->right);
+	else if (!node_to_remove->right)
+		transplant(&root, node_to_remove, node_to_remove->left);
+	else
+	{
+		temp = tree_minimum(node_to_remove->right);
+		if (temp->parent != node_to_remove)
+		{
+			transplant(&root, temp, temp->right);
+			temp->right = node_to_remove->right;
+			temp->right->parent = temp;
+		}
+		transplant(&root, node_to_remove, temp);
+		temp->left = node_to_remove->left;
+		temp->left->parent = temp;
+	}
 
-        /* Copy the value of the in-order successor to the root */
-        root->n = temp->n;
-
-        /* Remove the in-order successor from the right subtree */
-        root->right = bst_remove(root->right, temp->n);
-    }
-
-    return root;
+	free(node_to_remove);
+	return (root);
 }
